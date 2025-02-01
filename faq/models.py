bbb5521c13153caf846e8789391e8fc3bd99b4a3
AshuMishraG <aashutoshm77@gmail.com>
2025-02-01 23:09:00 +0530

    feat: Add automatic translation support using googletrans in FAQ model

--- faq/models.py
+++ faq/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from googletrans import Translator
 
 class FAQ(models.Model):
     question = models.TextField()
@@ -15,6 +16,18 @@ class FAQ(models.Model):
         elif lang == 'bn':
             return self.question_bn or self.question, self.answer_bn or self.answer
         return self.question, self.answer
+
+    def save(self, *args, **kwargs):
+        translator = Translator()
+        if not self.question_hi:
+            self.question_hi = translator.translate(self.question, dest='hi').text
+        if not self.question_bn:
+            self.question_bn = translator.translate(self.question, dest='bn').text
+        if not self.answer_hi:
+            self.answer_hi = translator.translate(self.answer, dest='hi').text
+        if not self.answer_bn:
+            self.answer_bn = translator.translate(self.answer, dest='bn').text
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.question
